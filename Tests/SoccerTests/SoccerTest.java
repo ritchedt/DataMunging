52d510bbbd63a1c9eb273bf6a9808665e9bc40c2
ritchedt <ritchedt@miamioh.edu>
2016-10-10 00:48:47 -0400

    Added code for Soccer Test
    Reason: Added a test case for Soccer test case. Failed on run as
    expected.
    
    Files changed:
    SoccerTest.java

--- Tests/SoccerTests/SoccerTest.java
+++ Tests/SoccerTests/SoccerTest.java
@@ -1,14 +1,17 @@
 package SoccerTests;
 
 import static org.junit.Assert.*;
-
+import java.io.IOException;
 import org.junit.Test;
+import SoccerClasses.Soccer;
+
 
 public class SoccerTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void getSmallestTemperatureSpread() throws IOException {
+		Soccer test = new Soccer("Resources/football.dat");
+		assertTrue(test.getSmallestPointSpread() == -34);
 	}
 
 }
