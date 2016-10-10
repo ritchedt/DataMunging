78544805f7a67374d628cff6dc13b6d5a4c41f6a
ritchedt <ritchedt@miamioh.edu>
2016-10-09 20:05:57 -0400

    Able to read from .dat file
    Reason: Wrote a method called getSmallestTemperatureSpread to and able
    to read the top line from the .dat file
    
    Files Changed:
    Weather.java

--- JavaClasses/Weather/Weather.java
+++ JavaClasses/Weather/Weather.java
@@ -1,5 +1,29 @@
 package Weather;
 
-public class Weather {
+import java.io.*;
+import java.util.HashMap;
 
+
+public class Weather {
+	
+	String weatherFile;
+	
+	public Weather(String weatherFile){
+		this.weatherFile = weatherFile;
+	}
+		
+	public double getSmallestTemperatureSpread() throws IOException{
+		
+		BufferedReader reader = new BufferedReader(new FileReader(weatherFile));
+		HashMap columnName = new HashMap();
+		
+		String readLine = reader.readLine();
+		
+		
+		
+		reader.close();
+		
+		return 0;
+	}	
+	
 }
