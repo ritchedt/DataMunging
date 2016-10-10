b5e2c5795220ef643bcdde5c5bd4030f21f904f0
ritchedt <ritchedt@miamioh.edu>
2016-10-09 20:03:43 -0400

    Added Weather and Soccer test classes
    Reason: Provided a test case for Weather to have the test result be the
    expected number.
    
    Files changed:
    SoccerTest.java
    WeatherTest.java

--- Tests/SoccerTests/SoccerTest.java
+++ Tests/SoccerTests/SoccerTest.java
@@ -1,5 +1,14 @@
 package SoccerTests;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class SoccerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
 
 }
--- Tests/WeatherTests/WeatherTest.java
+++ Tests/WeatherTests/WeatherTest.java
@@ -1,5 +1,16 @@
 package WeatherTests;
 
+import static org.junit.Assert.*;
+import java.io.IOException;
+import org.junit.Test;
+import Weather.Weather;
+
 public class WeatherTest {
+
+	@Test
+	public void getSmallestTemperatureSpread() throws IOException {
+		Weather test = new Weather("Resources/weather.dat");
+		assertTrue(test.getSmallestTemperatureSpread() == 2);
+	}
 
 }
