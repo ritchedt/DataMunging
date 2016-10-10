b4f1c410018ff06ebd822068a3343fa22c7bd95e
ritchedt <ritchedt@miamioh.edu>
2016-10-09 20:11:45 -0400

    Populate HashMap with header values
    Reason: Populated HashMap with .dat header values with an index to later
    reference column number for collecting data.
    
    Files changed:
    Weather.java

--- JavaClasses/Weather/Weather.java
+++ JavaClasses/Weather/Weather.java
@@ -15,10 +15,17 @@ public class Weather {
 	public double getSmallestTemperatureSpread() throws IOException{
 		
 		BufferedReader reader = new BufferedReader(new FileReader(weatherFile));
-		HashMap columnName = new HashMap();
+		HashMap headerValue = new HashMap();
 		
 		String readLine = reader.readLine();
+		String regExWhiteSpace = "(\\s)+";
 		
+		
+		String[] getHeaderValues = readLine.split(regExWhiteSpace);
+		
+		for(int i=0; i<getHeaderValues.length; i++) {
+			headerValue.put(getHeaderValues[i],i);
+	    }
 		
 		
 		reader.close();
